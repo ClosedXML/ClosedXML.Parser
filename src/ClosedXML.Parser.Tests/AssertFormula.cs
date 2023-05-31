92f52166ff8865352e86fbfccfae593a0ce9197a
jahav <havlicek.honza@gmail.com>
2023-05-31 20:25:54 +0200

    Check lexer for failures, as well as parser.

--- src/ClosedXML.Parser.Tests/AssertFormula.cs
+++ src/ClosedXML.Parser.Tests/AssertFormula.cs
@@ -9,13 +9,30 @@ internal static class AssertFormula
     {
         var inputStream = new AntlrInputStream(formula);
         var lexer = new FormulaLexer(inputStream);
+        var listener = new LexerErrorListener();
+        lexer.RemoveErrorListeners();
+        lexer.AddErrorListener(listener);
         var commonTokenStream = new CommonTokenStream(lexer);
         var parser = new FormulaParser(commonTokenStream, TextWriter.Null, TextWriter.Null)
         {
-            Interpreter =  {  PredictionMode = PredictionMode.SLL  }
+            Interpreter = { PredictionMode = PredictionMode.SLL }
         };
+        parser.ErrorListeners.Clear();
+
         var res = parser.formula();
-        
-        Assert.IsNull(res.exception);
+
+        Assert.IsNull(listener.Errors, $"{formula}  {listener.Errors}");
+        Assert.IsNull(res.exception, $"{formula} {res.exception}");
+    }
+
+    private class LexerErrorListener : IAntlrErrorListener<int>
+    {
+        internal string? Errors { get; private set; }
+
+        public void SyntaxError(TextWriter output, IRecognizer recognizer, int offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        {
+            var error = $"line {line}:{charPositionInLine} {msg}";
+            Errors = Errors is null ? error : Errors + "\n" + error;
+        }
     }
 }
