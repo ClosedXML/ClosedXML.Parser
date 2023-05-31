60f06ae8f52e3a5630a70d6ecb25669fd19b589a
jahav <havlicek.honza@gmail.com>
2023-05-31 21:22:14 +0200

    Remove ambiguity of TRUE/FALSE as a constant and as a function. ANTLR will take the longer one.

---
+++ src/ClosedXML.Parser.Tests/FunctionTests.cs
@@ -0,0 +1,12 @@
+﻿namespace ClosedXML.Parser.Tests;
+
+[TestClass]
+public class FunctionTests
+{
+    [TestMethod]
+    [DataRow("TRUE(TRUE)")]
+    public void Ambiguous_built_in_function_name_is_recognized_as_function(string formula)
+    {
+        AssertFormula.CstParsed(formula);
+    }
+}
