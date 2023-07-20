57bcff5b34f030a357b4bede5a3e6df66b4ce46a
jahav <havlicek.honza@gmail.com>
2023-07-20 22:50:01 +0200

    Test class for ref_function rule wasn't public and thus wasn't evaluated.

---
+++ src/ClosedXML.Parser.Tests/Rules/RefFunctionCallRuleTests.cs
@@ -0,0 +1,19 @@
+﻿namespace ClosedXML.Parser.Tests.Rules;
+
+[TestClass]
+public class RefFunctionCallRuleTests
+{
+    [TestMethod]
+    public void Ref_functions_are_recognized()
+    {
+        var expected = new FunctionNode("IF") { Children = new AstNode[] { new ValueNode("Logical", true) } };
+        AssertFormula.SingleNodeParsed("IF(TRUE)", expected);
+    }
+
+    [TestMethod]
+    public void Ref_functions_can_have_whitespaces_around_braces()
+    {
+        var expected = new FunctionNode("CHOOSE") { Children = new AstNode[] { new ValueNode("Logical", true), new ValueNode("Number", 5.0) } };
+        AssertFormula.SingleNodeParsed("CHOOSE(  TRUE, 5  )", expected);
+    }
+}
