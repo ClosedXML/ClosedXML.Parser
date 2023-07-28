cc09bc6d073920f30abebe8e56dcea373772d41e
jahav <havlicek.honza@gmail.com>
2023-07-28 17:21:11 +0200

    Remove dead classes. CellArea was replaced by ReferenceArea and ExternalReferenceNode was split into three other nodes.

--- src/ClosedXML.Parser.Ast/ExternalReferenceNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClosedXML.Parser;
-
-public record ExternalReferenceNode(int WorkbookIndex, CellArea Reference) : AstNode
-{
-    public override string GetDisplayString()
-    {
-        return $"[{WorkbookIndex}]{Reference}!{Reference.GetDisplayString()}";
-    }
-};
