475e8b3ec4926d514e1e627ff87e1612c59000ec
jahav <havlicek.honza@gmail.com>
2023-07-29 03:40:00 +0200

    Replace ClosedXML.Parser.Web ASP.NET core app with a static web app and a single HTML page and an azure function.

---
+++ src/ClosedXML.Parser.Function/AstNodeConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ClosedXML.Parser.Function;
+
+internal class AstNodeConverter : JsonConverter<AstNode>
+{
+    private readonly ReferenceStyle _style;
+
+    internal AstNodeConverter(ReferenceStyle style)
+    {
+        _style = style;
+    }
+
+    public override AstNode ReadJson(JsonReader reader, Type objectType, AstNode existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        throw new NotSupportedException("Deserialization of AST is not supported.");
+    }
+
+    public override void WriteJson(JsonWriter writer, AstNode value, JsonSerializer serializer)
+    {
+        WriteNode(writer, value);
+    }
+
+    private void WriteNode(JsonWriter writer, AstNode value)
+    {
+        writer.WriteStartObject();
+        writer.WritePropertyName("type");
+        writer.WriteValue(value.GetTypeString());
+
+        writer.WritePropertyName("content");
+        writer.WriteValue(value.GetDisplayString(_style));
+
+        writer.WritePropertyName("children");
+        writer.WriteStartArray();
+        foreach (var child in value.Children)
+            WriteNode(writer, child);
+
+        writer.WriteEndArray();
+        writer.WriteEndObject();
+    }
+}
---
+++ src/ClosedXML.Parser.Function/ParseFormula.cs
@@ -0,0 +1,51 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace ClosedXML.Parser.Function
+{
+    public static class ParseFormula
+    {
+        [FunctionName("parse-formula")]
+        public static Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+        {
+            var refStyle = req.Query["style"] == "R1C1" ? ReferenceStyle.R1C1 : ReferenceStyle.A1;
+            string formulaText = req.Query["formula"];
+
+            var serializerSetting = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                Converters = { new AstNodeConverter(refStyle) }
+            };
+
+            try
+            {
+                var nodes = refStyle == ReferenceStyle.A1
+                    ? FormulaParser<ScalarValue, AstNode>.FormulaA1(formulaText, new F())
+                    : FormulaParser<ScalarValue, AstNode>.FormulaR1C1(formulaText, new F());
+                return Task.FromResult<IActionResult>(new JsonResult(new
+                {
+                    formula = formulaText,
+                    style = refStyle.ToString(),
+                    ast = nodes
+                }, serializerSetting));
+            }
+            catch (ParsingException e)
+            {
+                return Task.FromResult<IActionResult>(new JsonResult(new
+                {
+                    formula = formulaText,
+                    style = refStyle.ToString(),
+                    error = e.Message
+                }, serializerSetting)
+                {
+                    StatusCode = StatusCodes.Status422UnprocessableEntity
+                });
+            }
+        }
+    }
+}
