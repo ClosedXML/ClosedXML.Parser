4dc9719f2941ff9834805dd0383588af28c5a79a
jahav <havlicek.honza@gmail.com>
2024-01-09 22:12:11 +0100

    Establish constants for maximum row and column.

--- src/ClosedXML.Parser.Tests/Lexers/A1SpanReferenceTokenTests.cs
+++ src/ClosedXML.Parser.Tests/Lexers/A1SpanReferenceTokenTests.cs
@@ -11,28 +11,25 @@
 /// </summary>
 public class A1SpanReferenceTokenTests
 {
-    private const int MaxCol = 16384;
-    private const int MaxRow = 1048576;
-
     [Fact]
     public void Parse_row_range()
     {
         // Check A1_ROW ':' A1_ROW path
-        AssertAreaReferenceToken("1:1", new ReferenceArea(new RowCol(false, 1, true, 1, A1), new RowCol(false, 1, true, MaxCol, A1)));
-        AssertAreaReferenceToken("$5:10", new ReferenceArea(new RowCol(true, 5, true, 1, A1), new RowCol(false, 10, true, MaxCol, A1)));
-        AssertAreaReferenceToken("7:$3", new ReferenceArea(new RowCol(false, 7, true, 1, A1), new RowCol(true, 3, true, MaxCol, A1)));
-        AssertAreaReferenceToken("$1048576:$1048576", new ReferenceArea(new RowCol(true, 1048576, true, 1, A1), new RowCol(true, 1048576, true, MaxCol, A1)));
+        AssertAreaReferenceToken("1:1", new ReferenceArea(new RowCol(false, 1, true, 1, A1), new RowCol(false, 1, true, RowCol.MaxCol, A1)));
+        AssertAreaReferenceToken("$5:10", new ReferenceArea(new RowCol(true, 5, true, 1, A1), new RowCol(false, 10, true, RowCol.MaxCol, A1)));
+        AssertAreaReferenceToken("7:$3", new ReferenceArea(new RowCol(false, 7, true, 1, A1), new RowCol(true, 3, true, RowCol.MaxCol, A1)));
+        AssertAreaReferenceToken("$1048576:$1048576", new ReferenceArea(new RowCol(true, RowCol.MaxRow, true, 1, A1), new RowCol(true, RowCol.MaxRow, true, RowCol.MaxCol, A1)));
     }
 
     [Fact]
     public void Parse_column_range()
     {
         // Check A1_COLUMN ':' A1_COLUMN path
-        AssertAreaReferenceToken("A:A", new ReferenceArea(new RowCol(true, 1, false, 1, A1), new RowCol(true, MaxRow, false, 1, A1)));
-        AssertAreaReferenceToken("RW:ST", new ReferenceArea(new RowCol(true, 1, false, 491, A1), new RowCol(true, MaxRow, false, 514, A1)));
-        AssertAreaReferenceToken("$C:D", new ReferenceArea(new RowCol(true, 1, true, 3, A1), new RowCol(true, MaxRow, false, 4, A1)));
-        AssertAreaReferenceToken("E:$C", new ReferenceArea(new RowCol(true, 1, false, 5, A1), new RowCol(true, MaxRow, true, 3, A1)));
-        AssertAreaReferenceToken("$XFD:$XFD", new ReferenceArea(new RowCol(true, 1, true, MaxCol, A1), new RowCol(true, MaxRow, true, MaxCol, A1)));
+        AssertAreaReferenceToken("A:A", new ReferenceArea(new RowCol(true, 1, false, 1, A1), new RowCol(true, RowCol.MaxRow, false, 1, A1)));
+        AssertAreaReferenceToken("RW:ST", new ReferenceArea(new RowCol(true, 1, false, 491, A1), new RowCol(true, RowCol.MaxRow, false, 514, A1)));
+        AssertAreaReferenceToken("$C:D", new ReferenceArea(new RowCol(true, 1, true, 3, A1), new RowCol(true, RowCol.MaxRow, false, 4, A1)));
+        AssertAreaReferenceToken("E:$C", new ReferenceArea(new RowCol(true, 1, false, 5, A1), new RowCol(true, RowCol.MaxRow, true, 3, A1)));
+        AssertAreaReferenceToken("$XFD:$XFD", new ReferenceArea(new RowCol(true, 1, true, RowCol.MaxCol, A1), new RowCol(true, RowCol.MaxRow, true, RowCol.MaxCol, A1)));
     }
     
     private static void AssertAreaReferenceToken(string token, ReferenceArea expectedReference)
