c844c3519c6c002dba89ecffbd65ff577b2bbff2
jahav <havlicek.honza@gmail.com>
2024-01-17 20:44:30 +0100

    Expose visitor to modify formula.

---
+++ src/ClosedXML.Parser/ModContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClosedXML.Parser;
+
+/// <summary>
+/// A context for modifications of a formula through <see cref="CopyVisitor"/>.
+/// </summary>
+public class ModContext
+{
+    /// <summary>
+    /// Create a context for modifying formulas. 
+    /// </summary>
+    public ModContext(string formula, int row, int col, bool isA1)
+    {
+        if (string.IsNullOrWhiteSpace(formula))
+            throw new ArgumentException(nameof(formula));
+
+        if (row is < 1 or > RowCol.MaxRow)
+            throw new ArgumentOutOfRangeException(nameof(row));
+
+        if (col is < 1 or > RowCol.MaxCol)
+            throw new ArgumentOutOfRangeException(nameof(row));
+
+        Formula = formula;
+        Row = row;
+        Col = col;
+        IsA1 = isA1;
+    }
+
+    /// <summary>
+    /// The original formula without any modifications.
+    /// </summary>
+    public string Formula { get; }
+
+    /// <summary>
+    /// Absolute row number in a sheet.
+    /// </summary>
+    public int Row { get; }
+
+    /// <summary>
+    /// Absolute column number in a sheet.
+    /// </summary>
+    public int Col { get; }
+
+    /// <summary>
+    /// Should references in formula be A1?
+    /// </summary>
+    public bool IsA1 { get; }
+}
