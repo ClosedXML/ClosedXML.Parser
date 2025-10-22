af70adf6b044724f5a6e1b299c5ac5d3b5207cab
jahav <havlicek.honza@gmail.com>
2025-10-22 23:50:28 +0200

    Deduplicate code

--- src/ClosedXML.Parser/Pratt/Parselets/IdentParselet.cs
+++ src/ClosedXML.Parser/Pratt/Parselets/IdentParselet.cs
@@ -62,56 +62,25 @@ internal class IdentParselet<TScalar, T, TContext> : IPrefixParselet<T, TContext
             var bangToken = _parser.Consume(TokenType.Bang);
             var sheetWithBangRange = token.Range.ExtendRight(bangToken.Range);
 
-            if (_parser.LookAhead(1) is { Type: TokenType.Ident } sheetRefToken)
+            // No need to check for token type, if EoF, nothing will be matched to such token
+            var sheetRefToken = _parser.Consume();
+            
+            // Check for area `sheet!A1:B2` or just cell `sheet!A1`
+            // Check for colspan `sheet!A:B`
+            // Check for rowspan `sheet!1:2` with absolute or relative start row
+            if (_parser.TryReferenceA1(sheetRefToken, out var sheetArea, out var sheetAreaRange))
             {
-                _parser.Consume(TokenType.Ident);
-
-                // Check for area `sheet!A1:B2` or just cell `sheet!A1`
-                if (_parser.TryLocalAreaA1(sheetRefToken, out var sheetArea, out var sheetAreaRange))
-                {
-                    var range = sheetWithBangRange.ExtendRight(sheetAreaRange);
-                    var value = _factory.SheetReference(ctx, range, sheetName, sheetArea);
-                    return new Node<T>(value, range);
-                }
-
-                // Check for colspan `sheet!A:B`
-                if (_parser.TryLocalColSpanA1(sheetRefToken, out var sheetColSpan, out var sheetColSpanRange))
-                {
-                    var range = sheetWithBangRange.ExtendRight(sheetColSpanRange);
-                    var value = _factory.SheetReference(ctx, range, sheetName, sheetColSpan);
-                    return new Node<T>(value, range);
-                }
-
-                // Check for rowspan `sheet!$1:2` The $1 is an ident, but this doesn't detect
-                // rowspan starting with a relative row. That is checked below with a token number.
-                if (_parser.TryLocalRowSpanA1(sheetRefToken, out var sheetAbsRowSpan, out var sheetAbsRowSpanRange))
-                {
-                    var range = sheetWithBangRange.ExtendRight(sheetAbsRowSpanRange);
-                    var value = _factory.SheetReference(ctx, range, sheetName, sheetAbsRowSpan);
-                    return new Node<T>(value, range);
-                }
-
-                // Check for colspan `sheet!name`
-                if (_parser.TryGetName(sheetRefToken, out var name))
-                {
-                    var range = sheetWithBangRange.ExtendRight(sheetRefToken.Range);
-                    var value = _factory.SheetName(ctx, range, sheetName, name.ToString()); // String allocation, needed for the IAstFactory
-                    return new Node<T>(value, range);
-                }
-
-                throw new ParsingException($"Unable to parse value starting from position {token.Range.Start}.");
+                var range = sheetWithBangRange.ExtendRight(sheetAreaRange);
+                var value = _factory.SheetReference(ctx, range, sheetName, sheetArea);
+                return new Node<T>(value, range);
             }
 
-            // Check for rowspan `sheet!1:2` with relative start row
-            if (_parser.LookAhead(1).Type == TokenType.Number)
+            // Check for `sheet!name`
+            if (_parser.TryGetName(sheetRefToken, out var name))
             {
-                var sheetRowToken = _parser.Consume(TokenType.Number);
-                if (_parser.TryLocalRowSpanA1(sheetRowToken, out var sheetRowSpan, out var sheetRowSpanRange))
-                {
-                    var range = sheetWithBangRange.ExtendRight(sheetRowSpanRange);
-                    var value = _factory.SheetReference(ctx, range, sheetName, sheetRowSpan);
-                    return new Node<T>(value, range);
-                }
+                var range = sheetWithBangRange.ExtendRight(sheetRefToken.Range);
+                var value = _factory.SheetName(ctx, range, sheetName, name.ToString()); // String allocation, needed for the IAstFactory
+                return new Node<T>(value, range);
             }
 
             throw new ParsingException($"Unable to parse value starting from position {token.Range.Start}.");
